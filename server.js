6821e371beea3cf1d0284fcb7b7b785db3b41f07
Rodela Ghosh <ghoshrodela111@gmail.com>
2025-07-01 17:15:13 -0400

    Minor changes for render

--- server.js
+++ server.js
@@ -1,26 +1,25 @@
 import http from "node:http";
-
 import { serveStatic } from "./utils/serveStatic.js";
-import { handleGet } from "./routeHandlers/routeHandler.js";
-import { handlePost } from "./routeHandlers/routeHandler.js";
+import { handleGet, handlePost } from "./routeHandlers/routeHandler.js";
+import { fileURLToPath } from "url";
+import path from "path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const PORT = process.env.PORT || 2000;
-const __dirname = import.meta.dirname;
 
 const server = http.createServer(async(req, res)=>{
     if(req.url.startsWith("/api")){
         if(req.method==="GET"){
             return handleGet(res);
         }
-
         if(req.method==="POST"){
             return handlePost(req, res);
         }
-    }
-    
-    else{
+    } else {
         await serveStatic(req, res, __dirname);
     }
-})
+});
 
 server.listen(PORT, ()=>console.log(`Server running on port ${PORT}`));
-
--- utils/serveStatic.js
+++ utils/serveStatic.js
@@ -2,22 +2,25 @@ import path from "node:path";
 import fs from "node:fs/promises";
 import { getContentType } from "./getContentType.js";
 import { sendResponse } from "./sendResponse.js";
+import { parse } from "url";
+
 export async function serveStatic(req, res, basedir){
     const publicDir = path.join(basedir, "public");
-    const pathToResource = path.join(publicDir, 
-        req.url==="/" ? "index.html": req.url);
+
+    const parsedUrl = parse(req.url);
+    const pathname = parsedUrl.pathname;
+
+    const pathToResource = path.join(publicDir, pathname === "/" ? "index.html" : pathname);
 
     const ext = path.extname(pathToResource);
     const contentType = getContentType(ext);
 
     try{
         const content = await fs.readFile(pathToResource);
-        sendResponse( res, 200, contentType, content);
+        sendResponse(res, 200, contentType, content);
     }
-
     catch(err){
-        sendResponse( res, 404, "text/html", "Something went wrong");
+        sendResponse(res, 404, "text/html", "Something went wrong");
         console.log(err);
     }
-    
 }
